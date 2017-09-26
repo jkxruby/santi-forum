44f45ff77a7c1ee15c8899a9901e7d019bd5ae3d
jkxruby <jkxruby@gmail.com>
2017-09-26 16:50:55 +0800

    add index and new html

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,11 +5,12 @@ def index
 end
 
 def new
-  @group = group.new
+  @group = Group.new
 end
 
 def create
-  @group = group.new(group_params)
+  @group = Group.new(group_params)
+  @group.user = current_user 
   if @group.save
     redirect_to groups_path
   else
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-
   devise_for :users
-  root "welcome#index"
 
+  root "groups#index"
+
+  resources :groups
 
 end
