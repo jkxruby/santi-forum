6e9f21ed9e8575f5d0837a4fdfdad0405474729f
jkxruby <jkxruby@gmail.com>
2017-09-27 16:30:10 +0800

    add post controller and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,24 +2,28 @@ class PostsController < ApplicationController
 before_action :authenticate_user!
 
 def new
-  @group = Group.find(params[:id])
-  @post.group = @group
+  @group = Group.find(params[:group_id])
+
   @post = Post.new
 end
 
 def create
-  @group = Group.find(params[:id])
+  @group = Group.find(params[:group_id])
+
+  @post = Post.new(post_params)
   @post.group = @group
   @post.user = current_user
-  @post = Post.new(post_params)
   if @post.save
-    redirect_to group_path(group),notice: "ok, new post"
+    redirect_to group_path(@group), notice: "ok, new post"
   else
     render :new
   end
 end
 
-
+private
+def post_params
+  params.require(:post).permit(:content)
+end
 
 
 end
